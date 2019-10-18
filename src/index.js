4b1205c88d21cf3543a0e290cb29e07ce684b498
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-10-17 22:33:04 -0600

    nav and store created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './containers/App'
+import * as serviceWorker from './serviceWorker'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { reducer } from './services/reducers.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger()
+export const store = createStore(reducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
